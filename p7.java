e80f43745a66ca4249ecc1d0c4faa2104ce0db75
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-08-06 17:03:59 +0530

    Create p7.java

---
+++ p7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	        Scanner s=new Scanner(System.in);
+	        int a=s.nextInt(),b=s.nextInt(),temp;
+	        temp=a;a=b;b=temp;
+		System.out.println(a+" "+b);
+	}
+}
