f341898d6a7212bd4af40211a804e4b042c3c760
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-08-06 17:21:12 +0530

    Update p9.java

--- p9.java
+++ p9.java
@@ -5,8 +5,13 @@ public class Main
 	public static void main(String[] args) {
 	        Scanner s=new Scanner(System.in);
 	        String a=s.nextLine();
-	        String[] b=a.split("\\s");
-		      Arrays.sort(b);
+	        String[] c=a.split("\\s");
+	        int[] b=new int[a.length()-1];
+	        for(int i=0;i<c.length;i++)
+	        {
+	                b[i]=Integer.parseInt(c[i]);
+	        }
+		Arrays.sort(b);
 	        System.out.print(b[b.length-1]);
 	}
 }
