aa851d3806ad895385e13e4516abc878cb0d62a0
Selvav096 <51014416+Selvav096@users.noreply.github.com>
2019-08-06 16:39:10 +0530

    Create p4.java

---
+++ p4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+        public static void main(String args[])
+        {
+                Scanner s=new Scanner(System.in);
+                String a=s.nextLine();
+                String[] b=a.split("\\.");
+                System.out.print(b.length);
+                
+        }
+}
